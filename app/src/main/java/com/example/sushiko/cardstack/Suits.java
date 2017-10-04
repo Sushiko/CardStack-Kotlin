1b601f98b3bf1ec8114592bb727074cfdd298a5d
Abe Choi <Abraham.Choi@rocketmail.com>
2017-10-04 16:23:57 -0700

    Update and rename Suits.java to Suits.kt

--- app/src/main/java/com/example/sushiko/cardstack/Suits.java
+++
@@ -1,7 +0,0 @@
-package com.example.sushiko.cardstack;
-
-/**
- * Created by Sushiko on 10/2/17.
- */
-
-public enum Suits {HEARTS, SPADES, CLOVERS, DIAMONDS}
---
+++ app/src/main/java/com/example/sushiko/cardstack/Suits.kt
@@ -0,0 +1,10 @@
+package com.example.sushiko.cardstack
+
+/**
+ * Created by Sushiko on 10/2/17.
+ */
+
+enum class Suits {
+    HEARTS, SPADES, CLOVERS, DIAMONDS
+}
+
